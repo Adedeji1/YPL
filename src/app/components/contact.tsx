cac1b9e4db1620cc83a076c2a6796632794b4ed5
Adedeji1 <afolabiadedeji110@yahoo.com>
2026-03-11 19:50:04 +0100

    creation of the Contact us Page

---
+++ src/app/components/contact.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Contact: React.FC = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>This is the Contact page placeholder.</p>
+    </div>
+  );
+};
