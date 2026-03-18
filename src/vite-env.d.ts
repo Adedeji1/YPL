561b0589791483253a4be304724410d918bf4403
Adedeji1 <afolabiadedeji110@yahoo.com>
2026-03-18 16:03:05 +0100

    FIXES to import issues

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,15 +1,26 @@
 /// <reference types="vite/client" />
 
-// custom asset types
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
 
-declare module '*.webp';
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.gif';
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
 
-declare module '*.svg' {
-  import * as React from 'react';
-  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.gif" {
+  const src: string;
+  export default src;
 }
